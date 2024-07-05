d57d9bc1f721fc188fc63bec1013f9b7489151d3
Sylvain Hallé <sylvainhalle@users.noreply.github.com>
2024-07-05 14:52:02 -0400

    Refactoring of one more class

--- Source/src/sensors/examples/CharacterizeSwappedEvents.java
+++ Source/src/sensors/examples/CharacterizeSwappedEvents.java
@@ -1,3 +1,20 @@
+/*
+    Processing of sensor events with BeepBeep
+    Copyright (C) 2023-2024 Sylvain Hallé
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package sensors.examples;
 
 import static ca.uqac.lif.cep.Connector.connect;
@@ -11,8 +28,6 @@ import ca.uqac.lif.cep.functions.Cumulate;
 import ca.uqac.lif.cep.functions.FunctionTree;
 import ca.uqac.lif.cep.functions.StreamVariable;
 import ca.uqac.lif.cep.io.Print;
-import ca.uqac.lif.cep.json.JPathFunction;
-import ca.uqac.lif.cep.json.StringValue;
 import ca.uqac.lif.cep.tmf.Filter;
 import ca.uqac.lif.cep.tmf.Fork;
 import ca.uqac.lif.cep.tmf.KeepLast;
@@ -20,15 +35,19 @@ import ca.uqac.lif.cep.tmf.Pump;
 import ca.uqac.lif.cep.tmf.Trim;
 import ca.uqac.lif.cep.util.Numbers;
 import ca.uqac.lif.fs.FileSystemException;
-import sensors.DateToTimestamp;
+import sensors.EventFormat;
 import sensors.LogRepository;
 import sensors.nears.JsonFeeder;
+import sensors.nears.NearsJsonFormat;
 import sensors.nears.NearsLogRepository;
 
 public class CharacterizeSwappedEvents
 {
 	/* The folder where the data files reside. */
 	protected static final LogRepository fs = new NearsLogRepository();
+	
+	/* The adapter for the event format. */
+	protected static EventFormat format = new NearsJsonFormat();
 	
 	public static void main(String[] args) throws FileSystemException, IOException
 	{
@@ -37,7 +56,7 @@ public class CharacterizeSwappedEvents
 		
 		JsonFeeder feeder = new JsonFeeder(is);
 		
-		ApplyFunction get_ts = new ApplyFunction(new FunctionTree(DateToTimestamp.instance, new FunctionTree(StringValue.instance, new JPathFunction("sentAt/$date"))));
+		ApplyFunction get_ts = new ApplyFunction(format.timestamp());
 		connect(feeder, get_ts);
 		Fork f1 = new Fork(2);
 		connect(get_ts, f1);
