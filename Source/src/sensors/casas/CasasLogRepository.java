4cd73c5f9c2c04218560eab00438fcca718efd56
Sylvain Hallé <sylvainhalle@users.noreply.github.com>
2024-08-19 09:05:52 -0400

    Auto-reformatting in Eclipse

--- Source/src/sensors/casas/CasasLogRepository.java
+++ Source/src/sensors/casas/CasasLogRepository.java
@@ -7,29 +7,29 @@ import java.io.InputStream;
 import sensors.LogRepository;
 
 /**
- * A file system that is open directly on the local folder where the data
- * file for the Casas dataset resides.
+ * A file system that is open directly on the local folder where the data file
+ * for the Casas dataset resides.
  */
 public class CasasLogRepository extends LogRepository
 {
 	/**
-	 * The folder name where the data file is located. 
+	 * The folder name where the data file is located.
 	 */
 	protected static final String FOLDER = "casas-data";
-	
+
 	public CasasLogRepository()
 	{
 		super(FOLDER);
 	}
-	
+
 	public CasasLogRepository(String sub_folder)
 	{
 		super(FOLDER + "/" + sub_folder);
 	}
-	
-	public InputStream readPart(String file, String start, String end) throws FileNotFoundException {
+
+	public InputStream readPart(String file, String start, String end) throws FileNotFoundException
+	{
 		InputStream is = new FileInputStream(file);
-		
 		return is;
 	}
 }
