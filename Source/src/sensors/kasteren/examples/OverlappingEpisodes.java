afe4a0f4499dbcfe8e897193b22d154752831eb0
Sylvain Hallé <1533065+sylvainhalle@users.noreply.github.com>
2024-11-29 16:21:44 -0500

    Kasteren overlapping episodes

--- Source/src/sensors/kasteren/examples/OverlappingEpisodes.java
+++ Source/src/sensors/kasteren/examples/OverlappingEpisodes.java
@@ -19,8 +19,19 @@ package sensors.kasteren.examples;
 
 import static ca.uqac.lif.cep.Connector.connect;
 
+import ca.uqac.lif.cep.GroupProcessor;
+import ca.uqac.lif.cep.functions.ApplyFunction;
+import ca.uqac.lif.cep.functions.Cumulate;
+import ca.uqac.lif.cep.functions.FunctionTree;
+import ca.uqac.lif.cep.functions.StreamVariable;
 import ca.uqac.lif.cep.io.Print;
+import ca.uqac.lif.cep.tmf.Fork;
+import ca.uqac.lif.cep.tmf.KeepLast;
 import ca.uqac.lif.cep.tmf.Pump;
+import ca.uqac.lif.cep.tmf.Slice;
+import ca.uqac.lif.cep.tmf.Trim;
+import ca.uqac.lif.cep.util.Booleans;
+import ca.uqac.lif.cep.util.Equals;
 import sensors.kasteren.KasterenEventFormat;
 import sensors.kasteren.KasterenEventFormat.KasterenFeeder;
 
@@ -32,9 +43,27 @@ public class OverlappingEpisodes
 	public static void main(String[] args)
 	{
 		KasterenFeeder feeder = s_format.getFeeder(null);
-		
+		Slice slice = new Slice(s_format.sensorId(), new GroupProcessor(1, 1) {{
+			Fork f = new Fork();
+			Trim t = new Trim(1);
+			connect(f, 1, t, 0);
+			ApplyFunction same = new ApplyFunction(new FunctionTree(Equals.instance,
+					new FunctionTree(s_format.stateString(), StreamVariable.X),
+					new FunctionTree(s_format.stateString(), StreamVariable.Y)
+					));
+			connect(f, 0, same, 0);
+			connect(t, 0, same, 1);
+			Cumulate or = new Cumulate(Booleans.or);
+			connect(same, or);
+			addProcessors(f, t, same, or);
+			associateInput(0, f, 0);
+			associateOutput(0, or, 0);
+		}});
+		connect(feeder, slice);
+		KeepLast last = new KeepLast();
+		connect(slice, last);
 		Pump p = new Pump();
-		connect(feeder, p);
+		connect(last, p);
 		Print print = new Print.Println();
 		connect(p, print);
 		
